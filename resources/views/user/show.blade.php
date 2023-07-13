64c35a67b26ec0a49fae9a5e5791b5a317eb751a
mgmuroki <mark.muroki@gmail.com>
2023-07-13 15:18:16 +0300

    Timestamp update

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -19,13 +19,30 @@
         </div>
     </div>
     <div class="row">
-        <div class="col mb-3">
-            <label class="form-label">Created At</label>
-            <input type="text" name="created_at" class="form-control" placeholder="Created At" value="{{ $user->created_at }}" readonly>
-        </div>
-        <div class="col mb-3">
-            <label class="form-label">Updated At</label>
-            <input type="text" name="updated_at" class="form-control" placeholder="Updated At" value="{{ $user->updated_at }}" readonly>
-        </div>
-    </div>
+  <div class="col mb-3">
+    <label class="form-label">Created At</label>
+    <input id="created_at_input" type="text" name="created_at" class="form-control" placeholder="Created At" readonly>
+  </div>
+  <div class="col mb-3">
+    <label class="form-label">Updated At</label>
+    <input id="updated_at_input" type="text" name="updated_at" class="form-control" placeholder="Updated At" readonly>
+  </div>
+</div>
+
+<script>
+  // Retrieve the values from the server-side and convert them to Kenyan time
+  var createdAtValue = new Date("{{ $user->created_at }}");
+  var updatedAtValue = new Date("{{ $user->updated_at }}");
+  
+  // Set the time zone to Kenya (East Africa Time)
+  createdAtValue.setHours(createdAtValue.getHours() + 3); // UTC+3
+  updatedAtValue.setHours(updatedAtValue.getHours() + 3); // UTC+3
+  
+  // Convert the adjusted values to local time
+  var createdLocalTime = createdAtValue.toLocaleString("en-US", { timeZone: "Africa/Nairobi" });
+  var updatedLocalTime = updatedAtValue.toLocaleString("en-US", { timeZone: "Africa/Nairobi" });
+  
+  document.getElementById("created_at_input").value = createdLocalTime;
+  document.getElementById("updated_at_input").value = updatedLocalTime;
+</script>
   </x-app-layout>
